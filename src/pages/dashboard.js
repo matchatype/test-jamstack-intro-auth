944483beec3e3cb31d3f5e0440aa79a222d996ba
Andrea Di Marco <andrea@dimarco.io>
2020-02-22 11:43:49 -0500

    feat(dashboard): add working protected routes

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,28 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // noinspection NpmUsedModulesInstalled
 import { Router } from "@reach/router";
 import { navigate } from "gatsby";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
+import PrivateRoute from '../components/private-route'
 import Profile from "../components/profile";
 import RouteBase from "../components/route-base";
 import RouteLogin from "../components/route-login";
 import RouteSecret from "../components/route-secret";
+import 'react-netlify-identity-widget/styles.css'
 
 const Dashboard = ({ location }) => {
+  const [isVisible, setVisibility] = useState(false)
+
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate("/dashboard/login", { replace: true });
     }
   }, [location]);
 
+  const showModal = () => setVisibility(!isVisible)
+
   return (
     <Layout>
-      <Profile />
+      <Profile showModal={showModal} />
       <Router>
-        <RouteBase path="/dashboard/base" />
-        <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <PrivateRoute path="/dashboard/base" component={RouteBase} />
+        <PrivateRoute path="/dashboard/secret" component={RouteSecret} />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+      <IdentityModal showDialog={isVisible} onCloseDialog={showModal} />
     </Layout>
   );
 };
