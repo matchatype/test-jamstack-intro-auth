a75ee6686302b9741a473c945170a487cee70b75
Andrea Di Marco <andrea@dimarco.io>
2020-02-22 09:22:13 -0500

    feat(dashboard): add route components

---
+++ src/components/route-base.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const RouteBase = () => <h1>All of your base belong to us!</h1>
+
+export default RouteBase
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, { useEffect } from "react";
+// noinspection NpmUsedModulesInstalled
+import { Router } from "@reach/router";
+import { navigate } from "gatsby";
+import Layout from "../components/layout";
+import Profile from "../components/profile";
+import RouteBase from "../components/route-base";
+import RouteLogin from "../components/route-login";
+import RouteSecret from "../components/route-secret";
 
-const Dashboard = () => {
+const Dashboard = ({ location }) => {
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\/?$/)) {
+      navigate("/dashboard/login", { replace: true });
+    }
+  }, [location]);
+
   return (
     <Layout>
-      <p>TODO: create a dashboard</p>
+      <Profile />
+      <Router>
+        <RouteBase path="/dashboard/base" />
+        <RouteSecret path="/dashboard/secret" />
+        <RouteLogin path="/dashboard/login" />
+      </Router>
     </Layout>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
